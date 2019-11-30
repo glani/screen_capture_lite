ad702a8c5e3f9b5efb800445c3c70f6c4691a361
deniska <denis.yesakov@gmail.com>
2019-11-30 22:55:16 +0100

    minor changes

--- include/linux/X11FrameProcessor.h
+++ include/linux/X11FrameProcessor.h
@@ -23,7 +23,6 @@ namespace SL {
             void Pause() {}
             void Resume() {}
             DUPL_RETURN Init(std::shared_ptr<Thread_Data> data, Monitor& monitor);
-            DUPL_RETURN InitSync(std::shared_ptr<Sync_Data> data, Monitor& monitor);
             DUPL_RETURN ProcessFrame(const Monitor& currentmonitorinfo);
             DUPL_RETURN Init(std::shared_ptr<Thread_Data> data, const Window& selectedwindow);
             DUPL_RETURN ProcessFrame(Window& selectedwindow);
