6f114e7cf28e4f7f1c26f08747794a61338cf35a
des <des@norcom.de>
2019-12-01 19:03:47 +0100

    ios build done

---
+++ include/ios/NSFrameProcessorSync.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "internal/SCCommon.h"
+
+namespace SL {
+    namespace Screen_Capture {
+        class NSFrameProcessorSync;
+        struct NSFrameProcessorSyncImpl;
+        NSFrameProcessorSyncImpl* CreateNSFrameProcessorSyncImpl();
+        void DestroyNSFrameProcessorSyncImpl(NSFrameProcessorSyncImpl*);
+        void setMinFrameDurationSync(NSFrameProcessorSyncImpl*, const std::chrono::microseconds& );
+        
+        DUPL_RETURN Init(NSFrameProcessorSyncImpl* createdimpl, NSFrameProcessorSync* parent);
+        
+        class NSFrameProcessorSync : public BaseFrameProcessorSync {
+            NSFrameProcessorSyncImpl* NSFrameProcessorSyncImpl_ = nullptr;
+        public:
+            NSFrameProcessorSync();
+            ~NSFrameProcessorSync();
+            Monitor SelectedMonitor;
+            std::mutex okMutex;
+            std::condition_variable okCondition;
+            bool ok;
+            DUPL_RETURN InitSync(std::shared_ptr<Sync_Data> data, Monitor& monitor);
+            DUPL_RETURN ProcessFrame(const Monitor& curentmonitorinfo);
+            
+        };
+    }
+}
---
+++ src/ios/NSFrameProcessorSync.mm
@@ -0,0 +1,140 @@
+#include "NSFrameProcessorSyncm.h"
+
+@implementation FrameProcessorSync
+
+- (SL::Screen_Capture::DUPL_RETURN)Init:(SL::Screen_Capture::NSFrameProcessorSync *)parent {
+    self = [super init];
+    if (self) {
+        self.nsframeprocessor = parent;
+        self.avcapturesession = [[AVCaptureSession alloc] init];
+
+        self.avinput = [[[AVCaptureScreenInput alloc] initWithDisplayID:SL::Screen_Capture::Id(
+                parent->SelectedMonitor)] autorelease];
+        [self.avcapturesession addInput:self.avinput];
+
+        self.output = [[AVCaptureVideoDataOutput alloc] init];
+        NSDictionary *videoSettings = [NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithUnsignedInt:kCVPixelFormatType_32BGRA], (id) kCVPixelBufferPixelFormatTypeKey, nil];
+
+        [self.output setVideoSettings:videoSettings];
+        [self.output setAlwaysDiscardsLateVideoFrames:true];
+        //partial capture needed
+        if (parent->SelectedMonitor.OffsetX != parent->SelectedMonitor.OriginalOffsetX ||
+            parent->SelectedMonitor.OffsetY != parent->SelectedMonitor.OriginalOffsetY ||
+            parent->SelectedMonitor.Height != parent->SelectedMonitor.OriginalHeight ||
+            parent->SelectedMonitor.Width != parent->SelectedMonitor.OriginalWidth) {
+            CGRect r;
+            r.origin.x = parent->SelectedMonitor.OffsetX;
+            //apple uses the opengl texture coords where the bottom left is 0,0
+            r.origin.y = parent->SelectedMonitor.OriginalHeight -
+                         ((parent->SelectedMonitor.OffsetY - parent->SelectedMonitor.OriginalOffsetY) +
+                          parent->SelectedMonitor.Height);
+            r.size.height = parent->SelectedMonitor.Height;
+            r.size.width = parent->SelectedMonitor.Width;
+            [self.avinput setCropRect:r];
+        }
+        CMTime interval = CMTimeMake(1, 1);
+        [self.avinput setMinFrameDuration:interval];
+
+
+        self.avinput.capturesCursor = false;
+        self.avinput.capturesMouseClicks = false;
+
+        [self.avcapturesession addOutput:self.output];
+        [self.output setSampleBufferDelegate:self queue:dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0)];
+        [self.avcapturesession startRunning];
+
+        return SL::Screen_Capture::DUPL_RETURN::DUPL_RETURN_SUCCESS;
+    }
+    return SL::Screen_Capture::DUPL_RETURN::DUPL_RETURN_ERROR_UNEXPECTED;
+}
+
+- (void)dealloc {
+    [self.avcapturesession stopRunning];
+    [self.output release];
+    [self.avinput release];
+    [self.avcapturesession release];
+    [super dealloc];
+}
+
+- (void)setFrameRate:(CMTime)interval {
+    [self.avinput setMinFrameDuration:interval];
+}
+
+- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection {
+
+    if (!self.avcapturesession.isRunning) {
+        return;
+    }
+    auto data = self.nsframeprocessor->Data;
+    auto &selectedmonitor = self.nsframeprocessor->SelectedMonitor;
+
+    auto imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer);
+    CVPixelBufferLockBaseAddress(imageBuffer, kCVPixelBufferLock_ReadOnly);
+    auto bytesperrow = CVPixelBufferGetBytesPerRow(imageBuffer);
+    auto buf = static_cast<unsigned char *>(CVPixelBufferGetBaseAddress(imageBuffer));
+    SL::Screen_Capture::ProcessCapture(data->ScreenCaptureData, *(self.nsframeprocessor), selectedmonitor, buf,
+                                       bytesperrow);
+    CVPixelBufferUnlockBaseAddress(imageBuffer, kCVPixelBufferLock_ReadOnly);
+    [self.avcapturesession stopRunning];
+    std::unique_lock<std::mutex> _(self.nsframeprocessor->okMutex);
+    self.nsframeprocessor->ok = true;
+    self.nsframeprocessor->okCondition.notify_one();
+    _.unlock();
+}
+@end
+
+namespace SL {
+    namespace Screen_Capture {
+
+        struct NSFrameProcessorSyncImpl {
+            FrameProcessorSync *ptr = nullptr;
+
+            NSFrameProcessorSyncImpl() {
+                ptr = [[FrameProcessorSync alloc] init];
+            }
+
+            ~NSFrameProcessorSyncImpl() {
+                if (ptr) {
+                    [ptr release];
+                    auto r = CFGetRetainCount(ptr);
+                    while (r != 1) {
+                        r = CFGetRetainCount(ptr);
+                        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+                    }
+                    ptr = nullptr;
+                }
+            }
+
+            void setMinFrameDurationSync(const std::chrono::microseconds &duration) {
+                [ptr setFrameRate:CMTimeMake(1, 1)];
+            }
+
+            DUPL_RETURN Init(NSFrameProcessorSync *parent) {
+                return [ptr Init:parent];
+            }
+        };
+
+        NSFrameProcessorSyncImpl *CreateNSFrameProcessorSyncImpl() {
+            return new NSFrameProcessorSyncImpl();
+        }
+
+        void DestroyNSFrameProcessorSyncImpl(NSFrameProcessorSyncImpl *p) {
+            if (p) {
+                delete p;
+            }
+        }
+
+        void setMinFrameDurationSync(NSFrameProcessorSyncImpl *p, const std::chrono::microseconds &duration) {
+            if (p) {
+                p->setMinFrameDurationSync(duration);
+            }
+        }
+
+        DUPL_RETURN Init(NSFrameProcessorSyncImpl *createdimpl, NSFrameProcessorSync *parent) {
+            if (createdimpl) {
+                return createdimpl->Init(parent);
+            }
+            return DUPL_RETURN::DUPL_RETURN_ERROR_UNEXPECTED;
+        }
+    }
+}
